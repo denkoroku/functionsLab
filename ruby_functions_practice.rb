96d67d59dc496d0a658d36bb9c0225a54955cdd8
denkoroku <cathriddoch@gmail.com>
2019-09-03 16:08:12 +0100

    final commit

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -25,3 +25,39 @@ end
 def join_string (string1, string2)
   return string1 + string2
 end
+
+def add_string_as_number (string1, string2)
+  return string1.to_i + string2.to_i
+end
+
+def number_to_full_month_name (monthNumber)
+if monthNumber == 1
+  return "January"
+elsif monthNumber == 3
+  return "March"
+elsif monthNumber == 9
+  return "September"
+# else
+#   return "unknown"
+end
+end
+
+def number_to_short_month_name(monthNumber)
+  if monthNumber == 1
+    return "Jan"
+  elsif monthNumber == 4
+    return "Apr"
+  elsif monthNumber == 10
+    return "Oct"
+  # else
+  #   return "unknown"
+  end
+end
+
+def volume_of_cube(size)
+  return size ** 3
+end
+
+def volume_of_sphere(radius)
+  return (4/3)*(Math::PI)*(radius ** 3)
+end
